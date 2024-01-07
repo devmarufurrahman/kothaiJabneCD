ab9e13ee8481d9a36e07102f503b05327a94f163
devmarufurrahaman <mdmarufur5682@gmail.com>
2024-01-07 13:54:18 +0600

    registration ui design

---
+++ app/src/main/java/com/example/kothaijabencd/UserReg.java
@@ -0,0 +1,14 @@
+package com.example.kothaijabencd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserReg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_reg);
+    }
+}
